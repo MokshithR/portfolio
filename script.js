a49e82fdae3005b90cccf9cf2dffdca0e0adbdf8
MokshithR <mokshithr638@gmail.com>
2025-09-08 08:33:32 +0530

    Updated CSS and JS.

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-// Function to set scroll-padding-top dynamically
 function setScrollPadding() {
     const header = document.querySelector('header');
     if (header) {
         const headerHeight = header.offsetHeight;
-        document.documentElement.style.scrollPaddingTop = `${headerHeight + 20}px`; // Add a little extra space for aesthetics
+        document.documentElement.style.scrollPaddingTop = `${headerHeight + 20}px`;
     }
 }
 
-// Set the padding when the page loads
 setScrollPadding();
 
-// Recalculate padding on window resize to handle responsiveness
 window.addEventListener('resize', setScrollPadding);
 
-// Back to Top Button
 const backToTopBtn = document.createElement('button');
 backToTopBtn.id = 'backToTop';
 backToTopBtn.innerText = '↑';
