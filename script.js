5e77b8857b42df4b7ffd7ff91b1283715fc43319
MokshithR <mokshithr638@gmail.com>
2025-09-09 19:58:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuToggle = document.getElementById('menuToggle');
   const menu = document.getElementById('menu');
-
   menuToggle.addEventListener('click', () => {
     menu.classList.toggle('show');
     const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
     menuToggle.setAttribute('aria-expanded', !expanded);
   });
-
   const navLinks = menu.querySelectorAll('a');
   navLinks.forEach(link => {
     link.addEventListener('click', () => {
